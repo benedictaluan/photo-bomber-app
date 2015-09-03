e5602bddac0f20d031c477c27963956fe5c69092
Benedict Aluan <benedict.aluan@gmail.com>
2015-09-04 11:51:23 +1200

    Initial Commit

---
+++ Photo Bombers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Photo Bombers
+//
+//  Created by Benedict Aluan on 4/09/15.
+//  Copyright (c) 2015 Pencil Rocket. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Photo Bombers/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Photo Bombers
+//
+//  Created by Benedict Aluan on 4/09/15.
+//  Copyright (c) 2015 Pencil Rocket. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
